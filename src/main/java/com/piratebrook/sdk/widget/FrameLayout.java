e544a975a7d47ee8f6941bec7d1d837de9c4d0bc
piratebrook <779389629@qq.com>
2017-11-21 21:03:57 +0800

    temp

---
+++ src/main/java/com/piratebrook/sdk/widget/FrameLayout.java
@@ -0,0 +1,160 @@
+package com.piratebrook.sdk.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewDebug;
+import android.view.ViewGroup;
+import android.widget.RemoteViews;
+
+import com.piratebrook.sdk.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by wyy on 2017-11-21.
+ */
+@RemoteViews.RemoteView
+public class FrameLayout extends ViewGroup {
+
+    private static final int DEFAULT_CHILD_GRAVITY = Gravity.TOP | Gravity.START;
+
+    @ViewDebug.ExportedProperty(category = "measurement")
+    boolean mMeasureAllChildren = false;
+
+    @ViewDebug.ExportedProperty(category = "padding")
+    private int mForegroundPaddingLeft = 0;
+
+    @ViewDebug.ExportedProperty(category = "padding")
+    private int mForegroundPaddingTop = 0;
+
+    @ViewDebug.ExportedProperty(category = "padding")
+    private int mForegroundPaddingRight = 0;
+
+    @ViewDebug.ExportedProperty(category = "padding")
+    private int mForegroundPaddingBottom = 0;
+
+    private final ArrayList<View> mMatchParentChildren = new ArrayList<>(1);
+
+    public FrameLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public FrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FrameLayout(@NonNull Context context, @Nullable AttributeSet attrs,
+                       @AttrRes int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public FrameLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        final TypedArray a = context.obtainStyledAttributes(
+                attrs, R.styleable.FrameLayout, defStyleAttr, defStyleRes);
+
+        if (a.getBoolean(R.styleable.FrameLayout_measureAllChildren, false)) {
+            setMeasureAllChildren(true);
+        }
+
+        a.recycle();
+    }
+
+    /**
+     * Describes how the foreground is positioned. Defaults to START to TOP.
+     *
+     * @param foregroundGravity See {@link android.view..Gravity}
+     *
+     * @see #getForegroundGravity()
+     *
+     * @attr ref android.R.styleable#View_foregroundGravity
+     */
+    @Override
+    public void setForegroundGravity(int foregroundGravity) {
+        if (getForegroundGravity() != foregroundGravity) {
+            super.setForegroundGravity(foregroundGravity);
+        }
+
+        // calling get* again here because the set above may apply default constraints
+        final Drawable foreground = getForeground();
+        if (getForegroundGravity() == Gravity.FILL && foreground != null) {
+            Rect padding = new Rect();
+            if (foreground.getPadding(padding)) {
+                mForegroundPaddingLeft = padding.left;
+                mForegroundPaddingTop = padding.top;
+                mForegroundPaddingRight = padding.right;
+                mForegroundPaddingBottom = padding.bottom;
+            }
+        } else {
+            mForegroundPaddingLeft = 0;
+            mForegroundPaddingTop = 0;
+            mForegroundPaddingRight = 0;
+            mForegroundPaddingBottom = 0;
+        }
+
+        requestLayout();
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    }
+
+    int getPaddingLeftWithForeground() {
+        return isForegroundInsidePadding() ? Math.max(mPaddingRight, mForegroundPaddingLeft) :
+                mPaddingLeft + mForegroundPaddingLeft;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int count = getChildCount();
+
+        final boolean measureMatchParentChildren =
+                MeasureSpec.getMode(widthMeasureSpec) != MeasureSpec.EXACTLY ||
+                        MeasureSpec.getMode(heightMeasureSpec) != MeasureSpec.EXACTLY;
+        mMatchParentChildren.clear();
+
+        int maxHeight = 0;
+        int maxWidth = 0;
+        int childState = 0;
+
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
+            if (mMeasureAllChildren || child.getVisibility() != GONE) {
+                measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
+                final LayoutParams lp = child.getLayoutParams();
+                maxWidth = Math.max(maxWidth,
+                        child.getMeasuredWidth() + lp.le)
+            }
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+    /**
+     * Per-child layout information for layouts that support margins.
+     * See {@link android.R.styleable#FrameLayout_Layout FrameLayout Layout Attributes}
+     * for a list of all child view attributes that this class supports.
+     *
+     * @attr ref android.R.styleable#FrameLayout_Layout_layout_gravity
+     */
+    public static class LayoutParams extends MarginLayoutParams {
+        /**
+         * Value for {@link #gravity} indicating that a gravity has not been
+         * explicitly specified.
+         */
+        public static final int UNSPECIFIED_GRAVITY = -1;
+    }
+}
